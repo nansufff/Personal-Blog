2466d5998ccb3c1efbe5ff60fa42470ea47db884
Nandhana Suffin <nandhanasuffin20@gmail.com>
2024-07-22 11:20:58 +0400

    data model is being created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class blogpost(models.Model):
+    title=models.CharField(max_length=100)
+    excerpt=models.CharField(max_length=200)
+    image_name=models.CharField(max_length=100)
+    date=models.DateField(auto_now=True)
+    slug=models.SlugField(unique=True)
